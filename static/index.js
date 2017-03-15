1522832c50966ac5ff75ef085a5c336d07f4aab2
makomsh <matsui.mako@g.wakayama-u.jp>
2017-03-15 15:53:29 +0900

    未完のリスト表示

--- static/index.js
+++ static/index.js
@@ -1,3 +1,28 @@
 /**
  * Created by miyashita_ak on 17/01/20.
  */
+
+console.log("index.js");
+displayList();
+
+
+
+function displayList() {
+    $.ajax({
+        type: 'GET',
+        url: '../api/1/todo/',
+        success: function (json) {
+            // 成功時の処理
+            console.log(json);
+            $('#board').empty();
+            if (json.length != null) {
+                for (var i = 0; i < json.length; i++) {
+                    if (!json[i].done) {
+                        var mess = '<p>' + json[i].title + '</p>';
+                        $('#board').append(mess);
+                    }
+                }
+            }
+        }
+    });
+}
