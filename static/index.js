3afbce5d82ecc083496c44d1cd8263e665062768
makomsh <matsui.mako@g.wakayama-u.jp>
2017-03-16 18:18:48 +0900

    表示の切り替え実装
    デザインそろそろ検討しましょう

--- static/index.js
+++ static/index.js
@@ -1,41 +1,94 @@
 /**
  * Created by miyashita_ak on 17/01/20.
  */
+var Page = function(){
 
-console.log('index.js');
-displayList();
-$(document).on('click','#changeLists',function(){
+    //初期化
+    this.init = function(){
+        setList(false);
+    };
 
-    if(!$('#changeLists').data('done')){
-        console.log(!$('#changeLists').attr('data-done'));
-        $('#changeLists').attr('data-done','true');
-        $.ajax({
-            type: 'GET',
-            url: '../api/1/todo/',
-            success: function (json) {
-                // 成功時の処理
-                console.log(json);
-                $('#board').empty();
-                if (json.length != null) {
-                    for (var i = 0; i < json.length; i++) {
-                        if (json[i].done) {
-                            var mess = "<p><input type='checkbox' liDone='" +json[i].done +"'>"  + json[i].title + "<input type='checkbox' liStar='" + json[i].star +"'></p>";
-                            $('#board').append(mess);
+};
+//他に初期化しておくものがなければ
+//$(function())の中に直接書いてもよいかも？
+
+
+var p = new Page();
+var array =[];
+$(function(){
+    p.init();
+
+});
+
+function setList( doneMode ) {
+    console.log(doneMode);
+    $.ajax({
+        type: 'GET',
+        url: '../api/1/todo/',
+        success: function (json) {
+            // 成功時の処理;
+            $('#board').empty();
+            array = [];
+            if (json.length != null) {
+                for (var i = 0; i < json.length; i++) {
+                    var mess="";
+                    if(doneMode === "all") {
+                        array.push(json[i]);
+                    }else if (doneMode) {
+                        if(json[i].done) {
+                            array.push(json[i]);
                         }
+                    }else if(!json[i].done) {
+                        array.push(json[i]);
                     }
+                    $('#board').append(mess);
                 }
             }
-        });
-    }else{
-        $('#changeLists').attr('data-done','false');
-        displayList();
+            displayList(array);
+        }
+    });
+}
+
+function displayList( arrayList ){
+    $('#board').empty();
+    for(var i = 0 ; i < arrayList.length; i++) {
+        var mess = "<p value='" + arrayList[i].id + "'>";
+        mess += "<input type='checkbox' class='l_done"+arrayList[i].done +"' onclick='clickDoneBtn(this)'>";
+        mess += arrayList[i].title;
+        mess += "<input type='checkbox' class='l_star " + arrayList[i].star +"' onclick='clickStarBtn(this)'>" ;
+        mess += "<a>"+ arrayList[i].time_limit+"</a></p>";
+        $('#board').append(mess);
     }
+}
+
+
+$(document).on('click','#all',function() {setList("all")});
+$(document).on('click','#sort_non',function() {setList(false)});
+$(document).on('click','#sort_done',function() {setList(true)});
+$(document).on('click','#sort_star', function() {
+    for(var i = 0 ; i < array.length; i++) {
+        if(!array[i].star){
+            array.splice( i, 1 ) ;
+        }
+    }
+    displayList(array);
 });
-//test押下時
-$(document).on('click','#test', function() {
-console.log('create');
-//	console.log('comment'+ $('#message').val());
-    console.log($('#deadline').val());
+
+$(document).on('click','#sort_deadline', function() {
+    //締め切り順にソートするプログラム実行
+    array.sort(function(a,b){
+        if(a.time_limit < b.time_limit) return -1;
+        if(a.time_limit > b.time_limit) return 1;
+        return 0;
+    });
+    console.log(Object.keys(array));
+    displayList(array);
+});
+
+
+//create押下時
+$(document).on('click','#create', function() {
+
     $.ajax({
         type: 'POST',
         url: '../api/1/todo/',
@@ -55,31 +108,69 @@ console.log('create');
                 "title": $('#todo').val()
             }),
         success: function (json) {
-            displayList();
+            displayList(true);
         }
     });
-
-   // $('input[type="text"]').val('');
 });
 
-function displayList() {
+
+
+function clickDoneBtn(obj) {
+    //　どのTODOなのかを特定するための関数を作成する必要があり、引数はValue（Id)
+    //  var list= array[$(obj).parent().attr("value")-1];
+    var list= array[0];
     $.ajax({
-        type: 'GET',
-        url: '../api/1/todo/',
+        type: 'PUT',
+        url: '/api/1/todo/' + list.id,
+        contentType: 'application/json',
+        data: JSON.stringify(
+            {
+                "done": !list.done,
+                "star": list.star,
+                "tag_ids": [
+                    1
+                ],
+                "time_limit": list.time_limit,
+                "title": list.title
+            }),
         success: function (json) {
-            // 成功時の処理
-            console.log(json);
-            $('#board').empty();
-            if (json.length != null) {
-                for (var i = 0; i < json.length; i++) {
-                    if (!json[i].done) {
-                        var mess = "<p><input type='checkbox' class='l_done " +json[i].done +"'>";
-                        mess += json[i].title;
-                        mess += "<input type='checkbox' class='l_star " + json[i].star +"'></p>";
-                        $('#board').append(mess);
-                    }
-                }
-            }
+            setList(false);
         }
     });
+}
+
+function clickStarBtn(obj){
+//    var list= array[$(obj).parent().attr("value")-1];
+//　どのTODOなのかを特定するための関数を作成する必要があり、引数はValue（Id)
+    var list= array[0];
+
+    $.ajax({
+        type: 'PUT',
+        url: '/api/1/todo/' + list.id,
+        contentType: 'application/json',
+        data: JSON.stringify(
+            {
+                "done": list.done,
+                "star": !list.star,
+                "tag_ids": [
+                    1
+                ],
+                "time_limit": list.time_limit,
+                "title": list.title
+            }),
+        success: function (json) {
+            setList(false);
+        }
+    });
+}
+
+
+
+//どのTODOなのかを特定するための関数を作成する必要があり、引数はValue（Id)
+function searchList(value) {
+    var result =null;
+    var num = 0;
+
+
+    return num;
 }
