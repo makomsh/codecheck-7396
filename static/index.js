37defb079795cfecaaf52de5cc8b27f55a1246c8
makomsh <matsui.mako@g.wakayama-u.jp>
2017-03-20 23:56:58 +0900

    締め切り日の順に並び替え実装

--- static/index.js
+++ static/index.js
@@ -7,16 +7,12 @@ var Page = function(){
 
     //初期化
     this.init = function(){
- //       setList(false);
         $('#createNew').hide();
         changeMode("sort_non");
         display();
     };
 
 };
-//他に初期化しておくものがなければ
-//$(function())の中に直接書いてもよいかも？
-
 
 var p = new Page();
 var array =[];
@@ -26,6 +22,11 @@ $(function(){
     $( "#sortable" ).disableSelection();
 });
 
+/*
+* TODOLISTを表示する関数
+* 引数：なし
+* 返り値：なし
+* */
 function display() {
     $.ajax({
         type: 'GET',
@@ -36,9 +37,13 @@ function display() {
             changeDisplay();
         }
     });
-
 }
 
+/*
+* TODOLISTをセットする（changeDisplayにてモードに応じて表示切替）
+* 引数：TODOLIST一覧
+* 返り値：なし
+* */
 function setLists(arrayList) {
     $('#sortable').empty();
     for (var i = 0; i < arrayList.length; i++) {
@@ -98,41 +103,50 @@ function setLists(arrayList) {
 
 }
 
-
+/*
+*締め切り日まであと何日か計算するメソッド
+* 引数：締め切り日
+* 返り値：締め切り日までの日数
+* */
 function calTime(time){
     var end = new Date(time);
     var now = new Date();
     var day = Math.floor((end.getTime() - now.getTime())/(60*60*24*1000))+1;
-
     return day;
 }
 
-
-
+/*
+* 表示の仕方に応じて表示を変更するためのメソッド
+* 締め切り日の場合：
+*   すべてのTODOLISTを読み込みなおし、未完了のみ時間ごとにソート（締め切り日がない場合は下に表示）
+* すべて表示：TODOLISTの要素をすべて表示
+* それ以外：モードに応じてそれぞれの要素を表示・非表示する
+* 引数：なし
+* 返り値：なし
+*/
 function changeDisplay() {
     var mode = $('#sort_mode').attr('mode');
-
     var lists = $('#board').find('.panel-body');
-    if(mode != "sort_deadline") {
+    if (mode != "sort_deadline") {
         for (var i = 0; i < lists.length; i++) {
             var d = $('.task_done', lists[i]);
             var s = $('.task_star', lists[i]);
             var t = $('.task_day', lists[i]);
             var listParent = $(lists[i]).parent(".panel");
-            if(mode == "sort_done") {
-                if(d.is(':checked')){
+            if (mode == "sort_done") {
+                if (d.is(':checked')) {
                     listParent.show();
                 } else {
                     listParent.hide();
                 }
-            } else if(mode == 'sort_non'){
-                if(!d.is(':checked')){
+            } else if (mode == 'sort_non') {
+                if (!d.is(':checked')) {
                     listParent.show();
                 } else {
                     listParent.hide();
                 }
-            }else if (mode == "sort_star") {
-                if($(s).hasClass('glyphicon-star')) {
+            } else if (mode == "sort_star") {
+                if ($(s).hasClass('glyphicon-star')) {
                     listParent.show();
                 } else {
                     listParent.hide();
@@ -141,40 +155,57 @@ function changeDisplay() {
                 listParent.show();
             }
         }
-    }else{
-        //じかんごとに要素を並び替える。
-        //doneは排除。nullは下。締め切り近い順
+    } else {
+        $.ajax({
+            type: 'GET',
+            url: '../api/1/todo/',
+            success: function (json) {
+                // 成功時の処理;
+                var array=[];
+                for(var i = 0; i < json.length; i++){
+                    if(!json[i].done){
+                        array.push(json[i]);
+                    }
+                }
+                array.sort(function(a,b){
+                    if(a.time_limit == null) return 1; //nullは下へ
+                    if(a.time_limit < b.time_limit) return -1;
+                    if(a.time_limit > b.time_limit) return 1;
+                    return 0;
+                });
+                setLists(array);
+                changeDisplay();
+            }
+        });
     }
-
-
 }
 
 
-
-
-
-/*表示の仕方を変更するためのメソッド*/
-
+/*
+*表示の仕方を変更するためのメソッド
+* 前のモードをまず求めておく
+* モードを新しいものに設定
+* 前の並び方のボタンをdefaultに
+* 切り替える並び方のボタンをprimaryに
+* 引数：並び替えるモード
+* 返り値：なし
+* */
 function changeMode(mode) {
-
     var before = $('#sort_mode').attr('mode');
     $('#sort_mode').attr('mode', mode);
-
-
-
-
     $('#' + before).toggleClass('btn-primary');
     $('#' + before).toggleClass('btn-default');
     $('#' + mode).toggleClass('btn-primary');
     $('#' + mode).toggleClass('btn-default');
-
-
-
-    //前のボタンをdefaltに、新しいソートボタンをprimalyにclassチェンジ
-
 }
 
-
+/*
+* 並び替えボタンが押された時のイベント
+* １．モードの変更
+* ２．再表示
+* 引数：並び替える方法（モード）
+* 返り値：なし
+* */
 $(document).on('click','#all',function() {
     changeMode("all");
     changeDisplay();
@@ -183,7 +214,6 @@ $(document).on('click','#all',function() {
 $(document).on('click','#sort_non',function() {
     changeMode('sort_non');
     changeDisplay();
-
 });
 
 $(document).on('click','#sort_done',function() {
@@ -202,25 +232,30 @@ $(document).on('click','#sort_deadline', function() {
 });
 
 
-
-/*新しいタスクを作成するためのモーダル用ボタンのイベント*/
-
-
+/*
+*新しいタスクを作成するためのモーダル用ボタンのイベント
+* ＋New TO DO　が押された時はモーダルを表示
+* X　Canselが押された時はモーダルを非表示
+* */
 $(document).on('click','#createBtn',function() {
     $('#createNew').show();
 });
+
 $(document).on('click','#cansel',function() {
     $('#createNew').hide();
 });
+
 $(document).on('click','.close',function() {
     $('#createNew').hide();
 });
 
-
-//create押下時（タスクを作成するとき）
-
+/*
+* 新しいTODOを追加するためのメソッド
+* それぞれの情報をデータベースに格納
+* ＊カテゴリは０、Stringに設定
+* 表示したあとはモーダル非表示
+* */
 $(document).on('click','#create', function() {
-
     $.ajax({
         type: 'POST',
         url: '../api/1/todo/',
@@ -246,24 +281,16 @@ $(document).on('click','#create', function() {
     });
 });
 
-
-
-
 /*それぞれのTaskないのボタンのイベント*/
-
 /*完了・未完了きりかえ*/
 function clickDoneBtn(id) {
-
     var lists = $('#board').find('.panel-body');
-
     for (var i = 0; i < lists.length; i++) {
         if ( $(lists[i]).attr('value') == id ) {
-            console.log('same');
             var done = $('.task_done', lists[i]);
             var star = $('.task_star', lists[i]);
             var time_limit = $('.task_day', lists[i]).text();
             var title = $('.task_title', lists[i]).text();
-            console.log(title);
             $.ajax({
                 type: 'PUT',
                 url: '/api/1/todo/' + id,
@@ -282,8 +309,6 @@ function clickDoneBtn(id) {
                     display();
                 }
             });
-
-
             break;
         }
     }
@@ -294,9 +319,7 @@ function clickDoneBtn(id) {
 
 /*優先度の設定*/
 function clickStarBtn(id) {
-
     var lists = $('#board').find('.panel-body');
-
     for (var i = 0; i < lists.length; i++) {
         if ($(lists[i]).attr('value') == id) {
             console.log('same');
@@ -323,13 +346,10 @@ function clickStarBtn(id) {
                     display();
                 }
             });
-
-
             break;
         }
     }
 }
-
 
 /*削除ボタン*/
 function clickDeleteBtn(id){
